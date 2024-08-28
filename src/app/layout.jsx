983794fc3f383ce472eb8b2c30f9b92b6b5d2d98
saeyesss <shreyasnidugala@gmail.com>
2024-08-28 15:27:42 -0700

    change fonts and global css

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -1,7 +1,4 @@
-import { Inter } from 'next/font/google';
 import './globals.css';
-
-const inter = Inter({ subsets: ['latin'] });
 
 export const metadata = {
   title: 'D3 Demo - Shreyas Nidugala',
@@ -11,7 +8,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang='en'>
-      <body className={inter.className}>{children}</body>
+      <body className='font-sans bg-black text-white'>{children}</body>
     </html>
   );
 }
