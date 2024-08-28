4cc2da306a62253e82593bab5a4b8e7c6814eb59
saeyesss <shreyasnidugala@gmail.com>
2024-08-28 14:36:10 -0700

    tailwind.config.js: update colors and fonts

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,16 +1,22 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      colors: {
+        black: '#191414',
+        white: '#FFFFFF',
+        gray: '#282828',
+        darkgray: '#121212',
+        lightgray: '#B3B3B3',
+        green: '#1DB954',
+      },
+      fontFamily: {
+        sans: ['"Circular Std"', 'Helvetica', 'Arial', 'sans-serif'],
       },
     },
   },
