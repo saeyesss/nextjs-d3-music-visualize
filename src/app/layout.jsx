e1543e8c66dbded9f79e47eef28ef022a49c2d24
saeyesss <shreyasnidugala@gmail.com>
2024-09-24 21:06:22 -0700

    add analytics

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -2,11 +2,14 @@ import './globals.css';
 
 import Providers from '@/providers';
 
+import { Analytics } from '@vercel/analytics/react';
+
 export default function RootLayout({ children }) {
   return (
     <html lang='en'>
       <body className='font-sans bg-black text-white'>
         <Providers>{children}</Providers>
+        <Analytics mode={'production'} />;
       </body>
     </html>
   );
