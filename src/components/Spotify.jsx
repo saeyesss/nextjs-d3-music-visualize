67130d25a8c81a0c04c79b603f03f454203a10e1
saeyesss <shreyasnidugala@gmail.com>
2024-09-24 19:40:50 -0700

    Update Spotify.jsx

--- src/components/Spotify.jsx
+++ src/components/Spotify.jsx
@@ -6,23 +6,23 @@ import { useRouter, useSearchParams } from 'next/navigation';
 import { fetchSpotifyTokens } from '@/redux/slices/spotifySlice';
 
 const Spotify = () => {
-  const router = useRouter();
-  const dispatch = useDispatch();
-  const searchParams = useSearchParams();
-  useEffect(() => {
-    const code = searchParams.get('code');
-    const error = searchParams.get('error');
+  // const router = useRouter();
+  // const dispatch = useDispatch();
+  // const searchParams = useSearchParams();
+  // useEffect(() => {
+  //   const code = searchParams.get('code');
+  //   const error = searchParams.get('error');
 
-    if (error) {
-      console.log('Spotify authorization error', error);
-      router.push('/error');
-    } else if (code) {
-      console.log('got code fetching token');
-      dispatch(fetchSpotifyTokens(code));
-    }
-  }, [router, dispatch, searchParams]);
+  //   if (error) {
+  //     console.log('Spotify authorization error', error);
+  //     router.push('/error');
+  //   } else if (code) {
+  //     console.log('got code fetching token');
+  //     dispatch(fetchSpotifyTokens(code));
+  //   }
+  // }, [router, dispatch, searchParams]);
 
-  return <div className='text-white h-3'>Logging you in...</div>;
+  return <div className='text-white h-3'>Still in progress!</div>;
 };
 
 export default Spotify;
